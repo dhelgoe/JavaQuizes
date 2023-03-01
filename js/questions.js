fe43a1e7997a63b26aa6656512202937be7ead1f
derek helgoe <derek1896@gmail.com>
2023-03-01 14:58:39 -0600

    fixed changes, added questions

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: 'Lewis Hamilton won the first F1 world championship title when he raced for which team?',
+      choices: ['Mclaren', 'Mercedes', 'Red Bull', 'Haas'],
+      answer: 'Mclaren',
+    },
+    {
+      title: 'The title “Grand Prix” was first used for a motor race in which country?',
+      choices: ['Germany', 'France', 'United States', 'Poland'],
+      answer: 'France',
+    },
+    {
+      title: 'What is the nickname of Ferrari, one of the most successful racing teams in F1 history?',
+      choices: [
+        'The Dancing Donkey',
+        'The Prancing Horse',
+        'The Prancing Pony',
+        'The Dancing Unicorn',
+      ],
+      answer: 'The Prancing Horse',
+    },
+    {
+      title:
+        'Which driver won the first ever F1 world championship title?',
+      choices: ['Juan Manuel Fangio', 'Alberto Ascari', 'Giuseppe Farina', 'None of the above'],
+      answer: 'Giuseppe Farina',
+    },
+    {
+      title:
+        'For which country the Haas team compete?',
+      choices: ['United States', 'Italy', 'Germany', 'France'],
+      answer: 'United States',
+    },
+    {
+      title:
+        'The Hungarian Grand Prix became a part of the F1 world championship during which decade?',
+      choices: ['1940s', '1960s', '1980s', '2000s'],
+      answer: '1980s',
+    },
+    {
+      title:
+        'When a red and yellow striped flag is waved, the stripes will run in what direction?',
+      choices: ['Diagonally from left to right', 'Diagonally from right to left', 'Vertically', 'Theres no stripes'],
+      answer: 'Vertically',
+    },
+    {
+      title:
+        ' If a few drivers set identical lap times, then who would get the priority?',
+      choices: ['The driver with the most points', 'The driver with the most career wins', 'The driver who set first', 'The driver with the least points'],
+      answer: 'The driver who set first',
+    },
+    {
+      title:
+        'Which Race in 2005 saw just 6 cars start?',
+      choices: ['United States Grand Prix', 'Monaco Grand Prix', 'Bahrain Grand Prix', 'Australian Grand Prix'],
+      answer: 'United States Grand Prix',
+    },
+    {
+      title:
+        'Which circuit saw the first F1 Spring Race Trail',
+      choices: ['Brazil', 'Saudi Arabia', 'Silverstone', 'Bahrain'],
+      answer: 'Silverstone',
+    },
+    {
+      title:
+        'Which city hosted the Australian Grand Prix before Melbourne took over in 1996?',
+      choices: ['Adelaide', 'Melbourne', 'Sydney', 'Brisbane'],
+      answer: 'Adelaide',
+    },
+  ];
